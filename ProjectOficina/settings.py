0abce5c0b44b202156476e0044612536927c1304
blueIsaac1 <isaaccleitondasilva@gmail.com>
2024-11-05 08:56:04 -0300

    commit - changes

--- ProjectOficina/settings.py
+++ ProjectOficina/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'ProjectOficina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'oficina-main',
-        'USER': 'postgres',
-        'PASSWORD': 'ctdev',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'oficina-main',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ctdev',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
